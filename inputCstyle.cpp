87d4ac5e327b87079db0357d772c46983606d70e
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2025-02-07 12:18:14 +0530

    Add files via upload

---
+++ inputCstyle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout << "Enter a String: ";
+    cin >> str;
+    cout << "You Entered: " << str << endl;
+    cout << "\nEnter another String: ";
+    cin >> str;
+    cout << "You Enterned: " <<str << endl;
+    return 0;
+}
